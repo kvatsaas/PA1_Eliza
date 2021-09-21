3c311b0a7e9b22c909afc061ad3a6ec1365d11c0
vatsa008 <vatsa008@d.umn.edu>
2021-09-20 22:31:14 -0500

    Generally functional, in need of documentation and probably a few tweaks.

--- PA1 ELIZA/Eliza.cs
+++ PA1 ELIZA/Eliza.cs
@@ -5,19 +5,56 @@ using System.Text.RegularExpressions;
 
 namespace PA1_ELIZA
 {
-    class Eliza
+    static class Eliza
     {
-        static string input, name, major, home;
+        // Various self-explanatory variable creation
+        static string input = "";
+        static string name = "";
+        static string major = "";
+        static string home = "";
         static string log = "";
+        static string lastResponse = "";
 
-        static void GetInput()
+        // Preloaded basic questions
+        static string intro = "Hello! I am Eliza the Career Counselor, a chatbot written by Kristian Vatsaas.";
+        static string extro = "Goodbye and good luck in your future endeavors!";
+        static string askName = "What is your name?";
+        static string askMajor = "What is your major?";
+        static string askHome = "Where do you consider home?";
+        static string startConversation = "What can I help you with today?";
+        static string gibberishResponse = "I don't understand.";
+        static string abuseResponse = "Please keep a civil tongue and show respect!";
+        static int nameChance = 5;
+
+        static List<string> spurs = new List<string>()
+        {
+            "Have you considered academia?",
+            "Would you enjoy working in an office?",
+            "Do you have a preferred location?",
+            "Are you happy with your major?",
+            "Where do you see yourself in five years?"
+        };
+        static string basicSpur = "Do you have any other questions or concerns?";
+
+        static bool GetInput()
         {
             input = Console.ReadLine();
             log += input + "\n";
+            if (input.ToLower().Equals("exit") || input.ToLower().Equals("quit"))
+                return false;
+            input = ElizaPatterns.PronounSwap(input);
+            return true;
         }
 
-        static void Respond(string output, bool lastPrompt = true)
+        static void Respond(string output, bool lastPrompt = true, bool useName = true)
         {
+            lastResponse = output;
+            if (useName && !name.Equals(""))
+            {
+                var rand = new Random();
+                if (rand.Next(nameChance - 1) == 0)
+                    output = name + ", " + output.Substring(0, 1).ToLower() + output.Substring(1);
+            }
             output = "> " + output + "\n";
             if (lastPrompt)
                 output += "< ";
@@ -25,56 +62,190 @@ namespace PA1_ELIZA
             log += output;
         }
 
-        static void AskName()
+        static string GenerateResponse(bool spur = false)
         {
-            Respond("What is your name?");
-            GetInput();
-            name = ElizaPatterns.NameSpot(input);
+            string response;
+            foreach (string pattern in ElizaPatterns.keywords.Keys)
+            {
+                response = ElizaPatterns.KeywordSpot(input, pattern);
+                if (!response.Equals("") && !response.Equals(lastResponse))
+                    return response;
+            }
+            foreach (string pattern in ElizaPatterns.phrases.Keys)
+            {
+                response = ElizaPatterns.PhraseSpot(input, pattern);
+                if (!response.Equals("") && !response.Equals(lastResponse))
+                    return response;
+            }
+
+            if (spur)
+                return SpurResponse();
+            else return "";
+        }
+
+        static string SpurResponse()
+        {
+            if (spurs.Count == 0)
+                return basicSpur;
+            else
+            {
+                var rand = new Random();
+                var index = rand.Next(spurs.Count);
+                var response = spurs[index];
+                spurs.RemoveAt(index);
+                return response;
+            }
+        }
+
+        static void AddMajorToSpurs()
+        {
+            spurs.Add("You mentioned earlier that you are studying " + major + ". Do you like it?");
+            spurs.Add("What type of job would you like to have in the field of " + major + "?");
+            spurs.Add("Do you know anyone who already has a job in " + major + " that could help you?");
+        }
+
+        static void AddHomeToSpurs()
+        {
+            spurs.Add("Previously, you said " + home + " is your home. Do you want to stay there?");
+            spurs.Add("Did living in " + home + " affect your career planning?");
         }
 
         static void Main(string[] args)
         {
-            // This flag is set when ELIZA responds with something like "I don't know" to avoid doing so twice
-            var loopWarning = false;
+            var response = "";
+            bool skipPreload = false;
 
-            // Regex patterns to be used later
-            var i_my = @"I \b\w+\b my [^\p{P}]+";
-            var you_your = @"You \b\w+\b your [^\p{P}]+";
-            var we_our = @"We \b\w+\b our [^\p{P}]+";
-            Match match;
-                        
-            // Greeting
-            Respond("Hello! I am Eliza the Career Counselor, created by Kristian Vatsaas.", false);
+            // Introduce chatbot.
+            Respond(intro, false, false);
 
-            // Attempt to get user's name
-            AskName();
-            if (name == "")
-                Respond("That doesn't look like a name.");
+            /*
+             * Ask user for their name. If they give it, greet them and continue preloaded prompts.
+             * If not, attempt to match the input. If successful, skip preloaded prompts and start conversation using that input.
+             * Otherwise, continue preloaded prompts.
+             */
+            Respond(askName);
+            if (!GetInput())
+            {
+                Respond(extro, false, false);
+                return;
+            }
             else
-                Respond("Hello, " + name + "!");
-            
+            {
+                if (ElizaPatterns.IsAbusive(input))
+                    Respond(abuseResponse, true, false);
+                else if (ElizaPatterns.IsGibberish(input))
+                    Respond(gibberishResponse, true, false);
+                else
+                {
+                    name = ElizaPatterns.NameSpot(input);
+                    if (!name.Equals(""))
+                        Respond("Hello, " + name + "!", false, false);
+                    else if (!(response = GenerateResponse()).Equals(""))
+                        skipPreload = true;
+                }
+            }
+
+            /*
+             * Ask user for their major. If they give it, greet them and continue preloaded prompts.
+             * Also add associated prompts to potential conversation spurs.
+             * If not, attempt to match the input. If successful, skip preloaded prompts and start conversation using that input.
+             * Otherwise, continue preloaded prompts.
+             * Skip this step if user attempted to start a different conversation when asked for their name.
+             */
+
+            if (!skipPreload)
+            {
+                Respond(askMajor);
+                if (!GetInput())
+                {
+                    Respond(extro, false, true);
+                    return;
+                }
+                else
+                {
+                    if (ElizaPatterns.IsAbusive(input))
+                        Respond(abuseResponse, true, false);
+                    else if (ElizaPatterns.IsGibberish(input))
+                        Respond(gibberishResponse, true, false);
+                    else
+                    {
+                        major = ElizaPatterns.MajorSpot(input);
+                        if (!major.Equals(""))
+                            AddMajorToSpurs();
+                        else if (!(response = GenerateResponse()).Equals(""))
+                            skipPreload = true;
+                    }
+                }
+            }
+
+            /*
+             * Ask user for their home. If they give it, greet them and continue preloaded prompts.
+             * Also add associated prompts to potential conversation spurs.
+             * If not, attempt to match the input. If successful, skip preloaded prompts and start conversation using that input.
+             * Otherwise, continue preloaded prompts.
+             * Skip this step if user attempted to start a different conversation when asked for their name or major.
+             */
+
+            if (!skipPreload)
+            {
+                Respond(askHome);
+                if (!GetInput())
+                {
+                    Respond(extro, false, true);
+                    return;
+                }
+                else
+                {
+                    if (ElizaPatterns.IsAbusive(input))
+                        Respond(abuseResponse, true, false);
+                    else if (ElizaPatterns.IsGibberish(input))
+                        Respond(gibberishResponse, true, false);
+                    else
+                    {
+                        home = ElizaPatterns.HomeSpot(input);
+                        if (!home.Equals(""))
+                            AddHomeToSpurs();
+                        else if (!(response = GenerateResponse()).Equals(""))
+                            skipPreload = true;
+                    }
+                }
+            }
+
+            /*
+             * If response has already been generated from previous input, use that. 
+             * Otherwise, use the standard conversation starting prompt.
+             */
+            if (!skipPreload)
+                response = startConversation;
+            Respond(response);
+
             while (true)
             {
-                GetInput();
-
-                if (input.ToLower().Equals("exit"))
-                    break;
-                if (ElizaPatterns.IsAbusive(input))
+                if (!GetInput())
                 {
-                    Respond("Please be respectful and mind your manners.");
+                    Respond(extro, false, true);
+                    break;
+                }
+                else if (ElizaPatterns.IsAbusive(input))
+                {
+                    if (lastResponse.Equals(abuseResponse))
+                        Respond(SpurResponse());
+                    else
+                        Respond(abuseResponse, true, false);
                     continue;
                 }
-
-                input = ElizaPatterns.PronounSwap(input);
-
-                // Once one of these works, the condition is met and it stops trying, and the match is stored in match.Value
-                if ((match = Regex.Match(input, i_my, RegexOptions.IgnoreCase)).Success ||
-                    (match = Regex.Match(input, you_your, RegexOptions.IgnoreCase)).Success ||
-                    (match = Regex.Match(input, we_our, RegexOptions.IgnoreCase)).Success)
-                    Respond("Why do you say " + match.Value + "?");
+                else if (ElizaPatterns.IsGibberish(input))
+                {
+                    if (lastResponse.Equals(gibberishResponse))
+                        Respond(SpurResponse());
+                    else
+                        Respond(gibberishResponse, true, false);
+                    continue;
+                }
                 else
-                    Respond("Tell me more.");
+                    Respond(GenerateResponse());
             }
+
 
             // Quick and dirty method to echo the conversation to a file that I can print for PDF submission.
             // File.WriteAllText(@"S:\Downloads\log.txt", log);
--- PA1 ELIZA/ElizaPatterns.cs
+++ PA1 ELIZA/ElizaPatterns.cs
@@ -5,56 +5,71 @@ using System.Text.RegularExpressions;
 
 namespace PA1_ELIZA
 {
-    class ElizaPatterns
+    static class ElizaPatterns
     {
+
+        /*
+         * Any of the following will be considered gibberish:
+         * -An alphanumeric string with four consecutive vowels
+         * -An alphanumeric string with two separate instances of three consecutive vowels
+         * -An alphanumeric string with five consecutive consonants
+         * -An alphanumeric string with two separate instances of four consecutive consonants
+         * -An alphanumeric string with twenty or more characters
+         * This is obviously not particularly robust, but seems a decent rudimentary approach without requiring an extremely long or complex regex.
+         */
+        static readonly string rx_gibberish = @"\b[aeiou]{4,}\b|\b[aeiou]{3,}\w*[aeiou]{3,}\b|\b[bcdfghijklmnpqrstvwxyz]{5,}\b|\b[bcdfghijklmnpqrstvwxyz]{4,}\w*[bcdfghijklmnpqrstvwxyz]{4,}\b|\b[a-z]{20,}\b";
+
         /* Elementary regex that defines an elementary-age filter. More of a proof-of-concept to avoid turning in a program full of
          * slurs, so I didn't bother trying to capture all the compound words while avoiding words like 'scrap' or 'buttress'
          * as this filter is already only identifying a fairly arbitrary selection.
          */
         static readonly string rx_abuse = @"\bpoop\b|\bbutt\b|\bdummy\b|\bstupid\b|\bcrap\b|\bidiot\b";
 
+        static readonly string rx_name = @"(Your name( is|'s)|(You am( called)?)) (?<name>[\w'-]+)";
+        static readonly string rx_major = @"(Your (major|field|degree) is( in)?|You( am|'m) (in|studying)) (?<major>[\w' -]+)";
+        static readonly string rx_home = @"You ((lived?|grew up) in|am (from|living in)) (?<home>[\w' -\.]+\b)";
+
         // Regex pattern for pronouns
-        static readonly string rx_pronouns = @"\bI\b|\bme\b|\bmy\b|\byou\b|\byour\b|\bwe\b|\bour\b";
+        static readonly string rx_pronouns = @"\bi'm\b|\bi'll\b|\bi\b|\bme\b|\bmy\b|\byou are\b|\byou're\b|\byour\b|\byou\b|\bwe\b|\bour\b";
 
         // Dictionary for swapping pronouns. All keys are lowercase for simplified matching - candidates will be converted to lower.
         static readonly Dictionary<string, string> pronouns = new Dictionary<string, string>()
         {
             {"i", "you"},
+            {"i'm", "you am"},
+            {"i'll", "you'll"},
             {"me", "you"},
             {"my", "your"},
             {"you", "I"},
+            {"you are", "I are"},
+            {"you're", "I are"},
             {"your", "my"},
             {"we", "you"},
             {"our", "your"}
         };
 
-        static readonly Dictionary<string, string> keywords = new Dictionary<string, string>()
+        public static readonly Dictionary<string, string> keywords = new Dictionary<string, string>()
         {
             {@"\bresumes?\b", "Is your resume updated?"},
             {@"\bcareers?\b|\bjobs?\b", "Do you have a career path in mind?"},
-            {@"\bclass(es)?\b|\bcourses?\b|\bschool\b|\bgrades?\b", "Are your classes going well right now?"},
-            {@"research", "Are you interested in doing research?"},
-            {@"\b(grad|graduate) (program|degree|school|applications?|studies?)", "Do you want to pursue graduate studies?"}
+            {@"\b(grad|graduate) (program|degree|school|applications?|studies?)", "Do you want to pursue graduate studies?"},
+            {@"\bclass(es)?\b|\bcourses?\b|\bgrades?\b", "Are your classes going well right now?"},
+            {@"research", "Are you interested in doing research?"}
         };
 
-        static readonly Dictionary<string, string> phrases = new Dictionary<string, string>()
+        public static readonly Dictionary<string, string> phrases = new Dictionary<string, string>()
         {
-            {@"You (would like|want)", "Would <@1> further your goals?"},
-            {@"(can('t)?|could|will|would|won't) I", "Do you think if I <@1>, it will help your career planning?"},
-            {@"(What|Why) (is|are)", "I think it's more important to ask <@1> you think <@2> <@3>"},
-            {@"You [do(n't| not) know", "What is keeping you from finding out?"},
-            {@"", ""},
-            {@"", ""},
-            {@"", ""},
-            {@"", ""},
-            {@"", ""},
-            {@"", ""}
+            {@"^You(( would|'d) like|want) to (?<clause>[\w -']+)[!\?\.].*", "Would it further your goals to ${clause}?"},
+            {@"^(can|can't|could|will|would|won't|do|don't) I (?<clause>[\w -']+)[!\?\.].*", "Do you think if I ${clause}, it will help your career planning?"},
+            {@"^(?<option1>What|Why|Where) (?<option2>is|are) (?<word>[\w-']+) (?<clause>[\w -']+)[!\?\.].*", "First, ask yourself this: ${option1} do you think ${word} ${option2} ${clause}?"},
+            {@"^You do(n't| not) (?<option>know|think) (?<clause>[\w -']+)[!\?\.].*", "Why don't you ${option} ${clause}?"},
+            {@"^You am (?<clause>[\w -']+)[!\?\.].*", "Why are you ${clause}?"},
+            {@"^You am not (?<clause>[\w -']+)[!\?\.].*", "Why aren't you ${clause}?"},
+            {@"^You (?<option>can|cannot|can't|will|will not|won't|did|did not|didn't|do|do not|don't) (?<clause>[\w -']+)[!\?\.].*", "Why ${option} you ${clause}?"},
+            {@"^You (?<clause>[\w -']+)[!\?\.].*", "Why do you ${clause}?"},
+            {@"^I (?<clause>[\w -']+)[!\?\.].*", "Why do you think I ${clause}?"}
         };
 
-        static readonly string rx_name = @"(My name( is|'s)|((I am|I'm)( called)?)) (?<name>\b\w+\b)([ -]\b\w +\b)?";
-        static readonly string rx_major = @"(My (major|field|degree) is( in)?|(I am|I'm) (in|studying)) (?<major>[^\p{P}]*\b)";
-        static readonly string rx_home = @"I( (lived?|grew up) in|('m| am) (from|living in)) (?<home>(\b\w+\b)( \b\w+\b)?( \b\w+\b)?)";
-        static readonly string rx_oneWord = @"(?<phrase>\b\w+\b)";
 
         private static string WholeInputPattern(string input)
         {
@@ -73,11 +88,12 @@ namespace PA1_ELIZA
 
         private static string MaxWords(int max)
         {
-            var pattern = rx_oneWord;
+            var pattern = @"(?<phrase>\b\w+\b";
             for (int i = 0; i < max; i++)
             {
-                pattern += @"([ -]\b\w +\b)?";
+                pattern += @"([ -]\b\w+\b)?";
             }
+            pattern += ")";
             return pattern;
         }
 
@@ -91,20 +107,25 @@ namespace PA1_ELIZA
 
         public static bool IsAbusive(string input)
         {
-            return Regex.IsMatch(input, rx_abuse, RegexOptions.IgnoreCase);
+            return Regex.IsMatch(input, rx_abuse, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        }
+
+        public static bool IsGibberish(string input)
+        {
+            return Regex.IsMatch(input, rx_gibberish, RegexOptions.IgnoreCase | RegexOptions.Compiled);
         }
 
         public static string PronounSwap(string input)
         {
-            return Regex.Replace(input, rx_pronouns, m => pronouns[m.Value.ToLower()], RegexOptions.IgnoreCase);
+            return Regex.Replace(input, rx_pronouns, m => pronouns[m.Value.ToLower()], RegexOptions.IgnoreCase | RegexOptions.Compiled);
         }
 
         public static string NameSpot(string input)
         {
             Match match;
-            if ((match = Regex.Match(input, rx_name, RegexOptions.IgnoreCase)).Success)
+            if ((match = Regex.Match(input, rx_name, RegexOptions.IgnoreCase | RegexOptions.Compiled)).Success)
                 return match.Groups["name"].Value;
-            else if ((match = Regex.Match(input, MaxWords(2), RegexOptions.IgnoreCase)).Success)
+            else if ((match = Regex.Match(input, WholeInputPattern(MaxWords(2)), RegexOptions.IgnoreCase | RegexOptions.Compiled)).Success)
                 return match.Groups["phrase"].Value;
             else
                 return "";
@@ -113,9 +134,9 @@ namespace PA1_ELIZA
         public static string MajorSpot(string input)
         {
             Match match;
-            if ((match = Regex.Match(input, rx_major, RegexOptions.IgnoreCase)).Success)
+            if ((match = Regex.Match(input, rx_major, RegexOptions.IgnoreCase | RegexOptions.Compiled)).Success)
                 return match.Groups["major"].Value;
-            else if ((match = Regex.Match(input, MaxWords(4), RegexOptions.IgnoreCase)).Success)
+            else if ((match = Regex.Match(input, WholeInputPattern(MaxWords(4)), RegexOptions.IgnoreCase | RegexOptions.Compiled)).Success)
                 return match.Groups["phrase"].Value;
             else
                 return "";
@@ -124,28 +145,26 @@ namespace PA1_ELIZA
         public static string HomeSpot(string input)
         {
             Match match;
-            if ((match = Regex.Match(input, rx_home, RegexOptions.IgnoreCase)).Success)
+            if ((match = Regex.Match(input, rx_home, RegexOptions.IgnoreCase | RegexOptions.Compiled)).Success)
                 return match.Groups["home"].Value;
-            else if ((match = Regex.Match(input, MaxWords(3), RegexOptions.IgnoreCase)).Success)
+            else if ((match = Regex.Match(input, WholeInputPattern(MaxWords(3)), RegexOptions.IgnoreCase | RegexOptions.Compiled)).Success)
                 return match.Groups["phrase"].Value;
-            else
-                return "";
-        }
-
-        public static string PhraseSpot(string input, string pattern)
-        {
-            Match match;
-            if ((match = Regex.Match(input, rx_name, RegexOptions.IgnoreCase)).Success)
-                return match.Groups["home"].Value;
             else
                 return "";
         }
 
         public static string KeywordSpot(string input, string pattern)
         {
-            Match match;
-            if ((match = Regex.Match(input, rx_name, RegexOptions.IgnoreCase)).Success)
-                return match.Groups["home"].Value;
+            if (Regex.Match(input, pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled).Success)
+                return keywords[pattern];
+            else
+                return "";
+        }
+
+        public static string PhraseSpot(string input, string pattern)
+        {
+            if (Regex.Match(input, pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled).Success)
+                return Regex.Replace(input, pattern, phrases[pattern], RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
             else
                 return "";
         }
